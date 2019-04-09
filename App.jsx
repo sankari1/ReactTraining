1e646ec941a49c1e0ae4276ed62745b4d3712b2a
sankari1 <sankaridiamond96@gmail.com>
2019-04-09 09:19:11 +0530

    firstcommit

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+class App extends Component{
+   render(){
+      var age = 12;
+
+      return(
+         <div>
+            <h1>Hello World</h1>
+            <p>Sample program in react</p>
+            <div>{1+2}</div>
+            <div>{age>=18?"Eligible for Voting":"Not eligible for voting"}</div>
+         </div>
+      );
+   }
+}
+export default App;  
